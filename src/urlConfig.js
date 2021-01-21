c3bef46c2da2adb8199646c1dc7c02b88296c903
Josephboadi <boadijoseph151@gmail.com>
2021-01-21 10:17:46 +0100

    second commit

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,5 @@
-export const api = "http://localhost:2000/api";
+// export const api = "http://localhost:2000/api";
+export const api = "https://joy-flip-backend.herokuapp.com/api";
 export const genPublicUrl = (fileName) => {
   return `http://localhost:2000/public/${fileName}`;
 };
