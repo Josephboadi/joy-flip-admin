f9bd7c6271d4ef30e9f4c9d7295f3287c323616c
Josephboadi <boadijoseph151@gmail.com>
2021-01-21 10:30:31 +0100

    hopefull

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,7 @@
-// export const api = "http://localhost:2000/api";
-export const api = "https://joy-flip-backend.herokuapp.com/api";
+const baseUrl = process.env.API || "https://joy-flip-backend.herokuapp.com";
+//const baseUrl = "http://localhost:2000";
+
+export const api = `${baseUrl}/api`;
 export const genPublicUrl = (fileName) => {
   return `http://localhost:2000/public/${fileName}`;
 };
