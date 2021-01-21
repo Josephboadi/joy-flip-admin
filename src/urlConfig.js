d171648dc3867b2120f6184b193fbf56cf849dd5
Josephboadi <boadijoseph151@gmail.com>
2021-01-21 16:45:14 +0100

    hmmmmmm

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,5 @@
-// const baseUrl = process.env.API || "https://joy-flip-backend.herokuapp.com";
-const baseUrl = "http://localhost:2000";
+const baseUrl = "https://joy-flip-backend.herokuapp.com";
+// const baseUrl = "http://localhost:2000";
 
 export const api = `${baseUrl}/api`;
 export const genPublicUrl = (fileName) => {
